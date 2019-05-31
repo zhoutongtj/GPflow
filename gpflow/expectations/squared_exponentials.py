Fixing the precision errors in kernels

gpflow/expectations/squared_exponentials.py
@@ -3,10 +3,8 @@ import tensorflow as tf
 from .. import kernels
 from .. import mean_functions as mfn
 from ..features import InducingPoints
-
-from ..utilities.ops import square_distance
-from ..utilities.defaults import default_float
 from ..probability_distributions import DiagonalGaussian, Gaussian, MarkovGaussian
+from ..utilities.ops import square_distance
 from . import dispatch
 from .expectations import expectation
 
@@ -41,19 +39,17 @@ def _E(p, kernel, feature, _, __, nghp=None):
 
     lengthscale = kernel.lengthscale
     if not kernel.ard:
-        lengthscale = tf.zeros((D,), dtype=lengthscale.dtype) + kernel.lengthscale
+        lengthscale = tf.zeros((D, ), dtype=lengthscale.dtype) + kernel.lengthscale
 
-    chol_L_plus_Xcov = tf.linalg.cholesky(tf.linalg.diag(lengthscale ** 2) + Xcov)  # NxDxD
+    chol_L_plus_Xcov = tf.linalg.cholesky(tf.linalg.diag(lengthscale**2) + Xcov)  # NxDxD
 
     all_diffs = tf.transpose(Z) - tf.expand_dims(Xmu, 2)  # NxDxM
-    exponent_mahalanobis = tf.linalg.triangular_solve(chol_L_plus_Xcov, all_diffs,
-                                                      lower=True)  # NxDxM
+    exponent_mahalanobis = tf.linalg.triangular_solve(chol_L_plus_Xcov, all_diffs, lower=True)  # NxDxM
     exponent_mahalanobis = tf.reduce_sum(tf.square(exponent_mahalanobis), 1)  # NxM
     exponent_mahalanobis = tf.exp(-0.5 * exponent_mahalanobis)  # NxM
 
     sqrt_det_L = tf.reduce_prod(lengthscale)
-    sqrt_det_L_plus_Xcov = tf.exp(
-        tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chol_L_plus_Xcov)), axis=1))
+    sqrt_det_L_plus_Xcov = tf.exp(tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chol_L_plus_Xcov)), axis=1))
     determinants = sqrt_det_L / sqrt_det_L_plus_Xcov  # N
 
     return kernel.variance * (determinants[:, None] * exponent_mahalanobis)
@@ -74,14 +70,13 @@ def _E(p, mean, _, kernel, feature, nghp=None):
 
     lengthscale = kernel.lengthscale
     if not kernel.ard:
-        lengthscale = tf.zeros((D,), dtype=lengthscale.dtype) + lengthscale
+        lengthscale = tf.zeros((D, ), dtype=lengthscale.dtype) + lengthscale
 
-    chol_L_plus_Xcov = tf.linalg.cholesky(tf.linalg.diag(lengthscale ** 2) + Xcov)  # NxDxD
+    chol_L_plus_Xcov = tf.linalg.cholesky(tf.linalg.diag(lengthscale**2) + Xcov)  # NxDxD
     all_diffs = tf.transpose(feature.Z) - tf.expand_dims(Xmu, 2)  # NxDxM
 
     sqrt_det_L = tf.reduce_prod(lengthscale)
-    sqrt_det_L_plus_Xcov = tf.exp(
-        tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chol_L_plus_Xcov)), axis=1))
+    sqrt_det_L_plus_Xcov = tf.exp(tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chol_L_plus_Xcov)), axis=1))
     determinants = sqrt_det_L / sqrt_det_L_plus_Xcov  # N
 
     exponent_mahalanobis = tf.linalg.cholesky_solve(chol_L_plus_Xcov, all_diffs)  # NxDxM
@@ -91,8 +86,7 @@ def _E(p, mean, _, kernel, feature, nghp=None):
     exponent_mahalanobis = tf.reduce_sum(all_diffs * exponent_mahalanobis, 1)  # NxM
     exponent_mahalanobis = tf.exp(-0.5 * exponent_mahalanobis)  # NxM
 
-    return kernel.variance * (determinants[:, None] *
-                            exponent_mahalanobis)[:, None, :] * non_exponent_term
+    return kernel.variance * (determinants[:, None] * exponent_mahalanobis)[:, None, :] * non_exponent_term
 
 
 @dispatch.expectation.register(MarkovGaussian, mfn.Identity, NoneType, kernels.RBF, InducingPoints)
@@ -110,14 +104,13 @@ def _E(p, mean, _, kernel, feature, nghp=None):
     D = Xmu.shape[1]
     lengthscale = kernel.lengthscale
     if not kernel.ard:
-        lengthscale = tf.zeros((D,), dtype=lengthscale.dtype) + lengthscale
+        lengthscale = tf.zeros((D, ), dtype=lengthscale.dtype) + lengthscale
 
-    chol_L_plus_Xcov = tf.linalg.cholesky(tf.linalg.diag(lengthscale ** 2) + Xcov[0, :-1])  # NxDxD
+    chol_L_plus_Xcov = tf.linalg.cholesky(tf.linalg.diag(lengthscale**2) + Xcov[0, :-1])  # NxDxD
     all_diffs = tf.transpose(feature.Z) - tf.expand_dims(Xmu[:-1], 2)  # NxDxM
 
     sqrt_det_L = tf.reduce_prod(lengthscale)
-    sqrt_det_L_plus_Xcov = tf.exp(
-        tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chol_L_plus_Xcov)), axis=1))
+    sqrt_det_L_plus_Xcov = tf.exp(tf.reduce_sum(tf.math.log(tf.linalg.diag_part(chol_L_plus_Xcov)), axis=1))
     determinants = sqrt_det_L / sqrt_det_L_plus_Xcov  # N
 
     exponent_mahalanobis = tf.linalg.cholesky_solve(chol_L_plus_Xcov, all_diffs)  # NxDxM
@@ -127,12 +120,10 @@ def _E(p, mean, _, kernel, feature, nghp=None):
     exponent_mahalanobis = tf.reduce_sum(all_diffs * exponent_mahalanobis, 1)  # NxM
     exponent_mahalanobis = tf.exp(-0.5 * exponent_mahalanobis)  # NxM
 
-    return kernel.variance * (determinants[:, None] *
-                            exponent_mahalanobis)[:, None, :] * non_exponent_term
+    return kernel.variance * (determinants[:, None] * exponent_mahalanobis)[:, None, :] * non_exponent_term
 
 
-@dispatch.expectation.register((Gaussian, DiagonalGaussian), kernels.RBF, InducingPoints,
-                               kernels.RBF, InducingPoints)
+@dispatch.expectation.register((Gaussian, DiagonalGaussian), kernels.RBF, InducingPoints, kernels.RBF, InducingPoints)
 def _E(p, kern1, feat1, kern2, feat2, nghp=None):
     """
     Compute the expectation:
@@ -163,20 +154,18 @@ def _E(p, kern1, feat1, kern2, feat2, nghp=None):
     N = Xmu.shape[0]
     D = Xmu.shape[1]
 
-    squared_lengthscale = kernel.lengthscale ** 2
+    squared_lengthscale = kernel.lengthscale**2
     if not kernel.ard:
-        zero_lengthscale = tf.zeros((D,), dtype=squared_lengthscale.dtype)
+        zero_lengthscale = tf.zeros((D, ), dtype=squared_lengthscale.dtype)
         squared_lengthscale = squared_lengthscale + zero_lengthscale
 
-    sqrt_det_L = tf.reduce_prod(0.5 * squared_lengthscale) ** 0.5
+    sqrt_det_L = tf.reduce_prod(0.5 * squared_lengthscale)**0.5
     C = tf.linalg.cholesky(0.5 * tf.linalg.diag(squared_lengthscale) + Xcov)  # NxDxD
     dets = sqrt_det_L / tf.exp(tf.reduce_sum(tf.math.log(tf.linalg.diag_part(C)), axis=1))  # N
 
     C_inv_mu = tf.linalg.triangular_solve(C, tf.expand_dims(Xmu, 2), lower=True)  # NxDx1
-    C_inv_z = tf.linalg.triangular_solve(
-        C,
-        tf.tile(tf.expand_dims(tf.transpose(Z) / 2., 0), [N, 1, 1]),
-        lower=True)  # NxDxM
+    C_inv_z = tf.linalg.triangular_solve(C, tf.tile(tf.expand_dims(0.5 * tf.transpose(Z), 0), [N, 1, 1]),
+                                         lower=True)  # NxDxM
     mu_CC_inv_mu = tf.expand_dims(tf.reduce_sum(tf.square(C_inv_mu), 1), 2)  # Nx1x1
     z_CC_inv_z = tf.reduce_sum(tf.square(C_inv_z), 1)  # NxM
     zm_CC_inv_zn = tf.linalg.matmul(C_inv_z, C_inv_z, transpose_a=True)  # NxMxM
@@ -190,4 +179,4 @@ def _E(p, kern1, feat1, kern2, feat2, nghp=None):
     # Compute sqrt(self(Z)) explicitly to prevent automatic gradient from
     # being NaN sometimes, see pull request #615
     kernel_sqrt = tf.exp(-0.25 * square_distance(Z / kernel.lengthscale, None))
-    return kernel.variance ** 2 * kernel_sqrt * tf.reshape(dets, [N, 1, 1]) * exponent_mahalanobis
+    return kernel.variance**2 * kernel_sqrt * tf.reshape(dets, [N, 1, 1]) * exponent_mahalanobis

gpflow/kernels/stationaries.py
@@ -1,9 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
-from ..utilities.ops import broadcasting_elementwise, square_distance
-from ..utilities.defaults import default_float
 from ..base import Parameter, positive
+from ..utilities.ops import square_distance
 from .base import Kernel
 
 
@@ -18,11 +17,7 @@ class Stationary(Kernel):
     dimension, otherwise the kernel is isotropic (has a single lengthscale).
     """
 
-    def __init__(self,
-                 variance=1.0,
-                 lengthscale=1.0,
-                 active_dims=None,
-                 ard=None):
+    def __init__(self, variance=1.0, lengthscale=1.0, active_dims=None, ard=None):
         """
         - input_dim is the dimension of the input to the kernel
         - variance is the (initial) value for the variance parameter
@@ -57,7 +52,7 @@ class Stationary(Kernel):
         return self.K_r(r)
 
     def K_diag(self, X, presliced=False):
-        return tf.fill((X.shape[:-1]), tf.cast(tf.squeeze(self.variance), tf.float64))
+        return tf.fill((X.shape[:-1]), tf.squeeze(self.variance))
 
     def K_r(self, r):
         """
@@ -83,9 +78,9 @@ class RBF(Stationary):
     def K(self, X, X2=None, presliced=False):
         if not presliced:
             X, X2 = self.slice(X, X2)
-        X /= self.lengthscale
-        X2 = X2 / self.lengthscale if X2 is not None else X2
-        return self.variance * tf.exp(-square_distance(X, X2) / 2)
+        X_scaled = X / self.lengthscale
+        X2_scaled = X2 / self.lengthscale if X2 is not None else X2
+        return self.variance * tf.exp(-0.5 * square_distance(X_scaled, X2_scaled))
 
 
 class RationalQuadratic(Stationary):
@@ -102,17 +97,15 @@ class RationalQuadratic(Stationary):
     """
 
     def __init__(self, variance=1.0, lengthscale=1.0, alpha=1.0, active_dims=None, ard=None):
-        super().__init__(variance=variance, lengthscale=lengthscale, active_dims=active_dims,
-                         ard=ard)
+        super().__init__(variance=variance, lengthscale=lengthscale, active_dims=active_dims, ard=ard)
         self.alpha = Parameter(alpha, transform=positive())
 
     def K(self, X, X2=None, presliced=False):
         if not presliced:
             X, X2 = self.slice(X, X2)
-        X /= self.lengthscale
-        X2 = X2 / self.lengthscale if X2 is not None else X2
-        return self.variance * (1 + square_distance(X, X2) / (2 * self.alpha)) ** (
-            -self.alpha)
+        X_scaled = X / self.lengthscale
+        X2_scaled = X2 / self.lengthscale if X2 is not None else X2
+        return self.variance * (1 + 0.5 * square_distance(X_scaled, X2_scaled) / self.alpha)**(-self.alpha)
 
 
 class Exponential(Stationary):
@@ -171,7 +164,7 @@ class Matern52(Stationary):
 
     def K_r(self, r):
         sqrt5 = np.sqrt(5.)
-        return self.variance * (1.0 + sqrt5 * r + 5. / 3. * tf.square(r)) * tf.exp(-sqrt5 * r)
+        return self.variance * (1.0 + sqrt5 * r + 5.0 / 3.0 * tf.square(r)) * tf.exp(-sqrt5 * r)
 
 
 class Cosine(Stationary):